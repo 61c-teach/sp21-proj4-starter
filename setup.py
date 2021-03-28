08a9c94b1cfd19af7a9a0eada4a4823c2dab5858
Jie Qiu <jieqiu0630@gmail.com>
2021-03-28 12:56:09 -0700

    Raise not implemented error for setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
+    raise NotImplementedError
 
 if __name__ == "__main__":
     main()
